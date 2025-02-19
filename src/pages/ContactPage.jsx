7bdd491ab9c25612c084b6ab97049bd7e83c1509
Frengk Iskandar <frankierockabilly@gmail.com>
2025-02-19 08:47:57 +0700

    update

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import Contact from '../components/Contact'
 import Header from '../components/Header'
-import Footer from '../components/footer'
+import Footer from "../components/Footer";
 
 const ContactPage = () => {
   return (
-    <div>
-        <div className=' pt-5   bg-white overflow-hidden h-full relative'>
-            <div>
-                <Header />
-            </div>
+     <div>
+        <div className=" pt-5   bg-white overflow-hidden h-full relative">
+           <div>
+              <Header />
+           </div>
 
-
-            {/* isi gallery */}
-            <Contact />
+           {/* isi kontak */}
+           <Contact />
         </div>
         <div>
-                <Footer />
-            </div>
-    </div>
-  )
+           <Footer />
+        </div>
+     </div>
+  );
 }
 
 export default ContactPage
