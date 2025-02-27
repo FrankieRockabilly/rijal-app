9d4f1f7d71a7241410653a42ad16969de3656e3a
Frengk Iskandar <frankierockabilly@gmail.com>
2025-02-27 10:49:10 +0700

    fix bug error refresh halaman

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Agar dapat diakses dari perangkat lain
-    port: 5173,  // Pastikan port-nya 5173 (default Vite)
-  },
-})
+   plugins: [react()],
+   server: {
+      host: "0.0.0.0", // Agar dapat diakses dari perangkat lain
+      port: 5173, // Pastikan port-nya 5173 (default Vite)
+      historyApiFallback: true,
+   },
+});
 
